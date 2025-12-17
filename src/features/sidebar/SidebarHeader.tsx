fa47673d3090459789f815f294a474dea806f8b5
demitryedwards <dxyz773@gmail.com>
2025-12-17 15:47:31 -0500

    Add resume icons, updated resume bio

--- src/features/sidebar/SidebarHeader.tsx
+++ src/features/sidebar/SidebarHeader.tsx
@@ -1,7 +1,7 @@
 function SidebarHeader() {
   return (
     <header className="mt-32">
-      <h1 className="text-[45px] sm:text-[58px] lg:text-[50px] font-extrabold text-shadow max-w-[436px] dark:text-stone-200 leading-16 text-shadow-stone-50 flush-text tracking-wide">
+      <h1 className="text-[52px] sm:text-[58px] lg:text-[50px] font-extrabold text-shadow max-w-[436px] dark:text-stone-200 leading-16 text-shadow-stone-50 flush-text tracking-wide">
         <span className="bebas-neue-regular">Demitry Edwards</span>
       </h1>
       <div className="pt-3 flex flex-col max-w-[330px]">
